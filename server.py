c6c27fa6e3846cc92d94fd7e223971c09190e4f8
Prannay Khosla <prannay.khosla@gmail.com>
2016-06-24 18:01:25 +0530

    Keys and Certificate setup

--- server.py
+++ server.py
@@ -4,3 +4,8 @@ import sys
 import os
 
 app = Flask("HackingServer")
+app.secret_key = "hackingserversrocks"
+
+@app.route("/",methods=["GET"])
+def render_home():
+	return render_template('index.html')
